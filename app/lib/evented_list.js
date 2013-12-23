0c154ca18e22c173fe48a6e1356c1b3926552ac0
Benjamin West <ben@quickleft.com>
2013-12-23 14:51:21 -0700

    trickle events up

--- app/lib/evented_list.js
+++ app/lib/evented_list.js
@@ -6,12 +6,14 @@ _.extend(window.EventedList.prototype, Backbone.Events, {
   add: function(item){
     this.items.push(item);
     this.trigger('add', item);
+    this.listenTo(item, 'all', _.bind(this.trigger, this));
     return this.item;
   },
 
   remove: function(item){
     this.items = _.without(this.items, item);
     this.trigger('remove', item);
+    this.stopListening(item);
     return item;
   }
 
